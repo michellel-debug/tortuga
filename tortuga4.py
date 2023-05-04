8beee1a01aebf3148558959948afe21ec3c37bfd
michellel-debug <legorretamichelle80@gmail.com>
2023-05-03 18:12:49 -0600

    new file:   tortuga4.py

---
+++ tortuga4.py
@@ -0,0 +1,21 @@
+import turtle
+import time
+
+turtle.title("Tortuga")
+
+turtle.bgcolor('light green')
+
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+
+
+turtle.mainloop()
